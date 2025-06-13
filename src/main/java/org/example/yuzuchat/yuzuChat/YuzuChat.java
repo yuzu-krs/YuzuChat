3373f6ffa01bad6151f33fe15b688159bad401b4
yuzu-krs <yuzuto.poi@gmail.com>
2025-06-14 02:04:33 +0900

    init

---
+++ src/main/java/org/example/yuzuchat/yuzuChat/YuzuChat.java
@@ -0,0 +1,17 @@
+package org.example.yuzuchat.yuzuChat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class YuzuChat extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
